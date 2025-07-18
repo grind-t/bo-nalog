033e7875f316256973b3f147f3e2398fe300471b
grind-t <grindt.gd@gmail.com>
2025-07-18 23:23:38 +0300

    init

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  name: "@grind-t/lib",
+  name: "@grind-t/bo-nalog",
   entry: ["lib/index.ts"],
   outDir: "dist",
   format: ["esm", "cjs"],
