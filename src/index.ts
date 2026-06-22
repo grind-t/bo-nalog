0859fd08141417fce42611883467c5419f1d6c42
grind-t <grindt.gd@gmail.com>
2026-06-22 21:38:46 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from "./analytics/index.ts";
 export { getOrganizationBFO } from "./bfo/index.ts";
 export type { Correction } from "./bfo/schemas/correction/index.ts";
-export type { BFOResponse, BFOSchema } from "./bfo/schemas/index.ts";
+export type { BFO, BFOResponse } from "./bfo/schemas/index.ts";
 export { searchOrganizations } from "./search/index.ts";
 export type { SearchInput } from "./search/schemas/input.ts";
 export type { SearchOutput } from "./search/schemas/output.ts";
