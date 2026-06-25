0c7ef230f8f384b155fcb1d0c609bf7cfa478ea0
grind-t <grindt.gd@gmail.com>
2026-06-25 22:17:06 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { getOrganizationBFO } from "./bfo/index.ts";
 export type { Correction } from "./bfo/schemas/correction/index.ts";
 export type { BFO, BFOResponse } from "./bfo/schemas/index.ts";
+export { getBFOAnalysis } from "./bfo-analysis/index.ts";
+export type { BFOAnalysis } from "./bfo-analysis/schemas/index.ts";
 export { searchOrganizations } from "./search/index.ts";
 export type { SearchInput } from "./search/schemas/input.ts";
 export type { SearchOutput } from "./search/schemas/output.ts";
